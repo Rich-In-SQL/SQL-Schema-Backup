c2aded4887526a783d259ba4a0ebf4a39b29a66d
Rich In SQL <github@codenameowl.com>
2022-08-31 19:32:44 +0100

    Update SQL-Schema-Backup.ps1
    Amended script to accept params and log actions to text file along with the date of it's action.

--- SQL-Schema-Backup.ps1
+++ SQL-Schema-Backup.ps1
@@ -1,14 +1,19 @@
-﻿#[CmdletBinding()]
-#Param(
-#    [Parameter(Mandatory = $True)]
-#    [String] $database,
-#    [String] $server,
-#    [String] $SourceControlDirectory)
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True, Position=0, ValueFromPipeline=$false)]
+    [System.String]
+    $database,
+    [Parameter(Mandatory=$True, Position=0, ValueFromPipeline=$false)]
+    [System.String]
+    $server,
+    [Parameter(Mandatory=$True, Position=0, ValueFromPipeline=$false)]
+    [System.String]
+    $SourceControlDirectory,
+    [Parameter(Mandatory=$True, Position=0, ValueFromPipeline=$false)]
+    [System.String]
+    $logDirectory
+)
 
-$database = 'OffTheSprue'
-$server = 'localhost'
-$SourceControlDirectory = 'C:\Temp\'
-$logDirectory = 'C:\Temp\Logs\'
 $logName = 'SQL-Schema-Export-'
 $logFileName = $logName + (Get-Date -f yyyy-MM-dd-HH-mm) + ".log"
 $logFullPath =  Join-Path $logDirectory $logFileName
@@ -17,7 +22,7 @@ if(-Not(Test-Path -Path $logFullPath -PathType Leaf))
 {
     try {
     $null =  New-Item -ItemType File -Path $logFullPath -Force -ErrorAction Stop
-    Set-Content -Path $logFullPath -Value "The log file '$logFileName' has been created"
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - The log file '$logFileName' has been created"
 
     }
     catch {
@@ -29,94 +34,109 @@ $tablePath = $SourceControlDirectory + 'Tables'
 $storedProcedurePath = $SourceControlDirectory + 'StoredProcedures'
 $viewPath = $SourceControlDirectory + 'Views'
 
-if(!Get-Module -ListAvailable -name dbatools)
+if(Get-Module -ListAvailable -name dbatools)
 {
-    Install-Module dbatools -Confirm $false
-} else {
     Import-Module -Name dbatools
+} else {
+    Install-Module dbatools -Confirm $false
 }
 
 if (-not (Test-Path -LiteralPath $tablePath)) {
 
-    Set-Content -Path $logFullPath -Value  "Directory '$tablePath' doesn't exist, attempting to create."
+    Add-Content -Path $logFullPath -Value  "$(Get-Date -f yyyy-MM-dd-HH-mm) - Directory '$tablePath' doesn't exist, attempting to create." 
     
     try {
         New-Item -Path $tablePath -ItemType Directory -ErrorAction Stop | Out-Null
     }
     catch {
         Write-Error -Message "Unable to create directory '$tablePath'. Error was: $_" -ErrorAction Stop
-        Set-Content -Path $logFullPath -Value "Unable to create directory '$tablePath'. Error was: $_"
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to create directory '$tablePath'. Error was: $_"
     }
 
-    Set-Content -Path $logFullPath -Value "Successfully created directory '$tablePath'."
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Successfully created directory '$tablePath'."
 }
 else {
-    Set-Content -Path $logFullPath -Value "'$tablePath' already existed"
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - '$tablePath' already existed"
 }
 
 if (-not (Test-Path -LiteralPath $storedProcedurePath)) {
     
-    Set-Content -Path $logFullPath -Value  "Directory '$storedProcedurePath' doesn't exist, attempting to create." -ForegroundColor Yellow
+    Add-Content -Path $logFullPath -Value  "$(Get-Date -f yyyy-MM-dd-HH-mm) - Directory '$storedProcedurePath' doesn't exist, attempting to create." -ForegroundColor Yellow
 
     try {
         New-Item -Path $storedProcedurePath -ItemType Directory -ErrorAction Stop | Out-Null #-Force
     }
     catch {
         Write-Error -Message "Unable to create directory '$storedProcedurePath'. Error was: $_" -ErrorAction Stop
-        Set-Content -Path $logFullPath -Value "Unable to create directory '$storedProcedurePath'. Error was: $_" -ErrorAction Stop
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to create directory '$storedProcedurePath'. Error was: $_" -ErrorAction Stop
     }
     
-    Set-Content -Path $logFullPath -Value "Successfully created directory '$storedProcedurePath'." 
-
+    Add-Content -Path $logFullPath -Value "Successfully created directory '$storedProcedurePath'."
+ }
+ else {
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - '$storedProcedurePath' already existed"
 }
+
 
 if (-not (Test-Path -LiteralPath $viewPath)) {
     
-    Set-Content -Path $logFullPath -Value  "Directory '$viewPath' doesn't exist, attempting to create."
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Directory '$viewPath' doesn't exist, attempting to create."
 
     try {
         New-Item -Path $viewPath -ItemType Directory -ErrorAction Stop | Out-Null #-Force
     }
     catch {
         Write-Error -Message "Unable to create directory '$viewPath'. Error was: $_" -ErrorAction Stop
-        Set-Content -Path $logFullPath -Value "Unable to create directory '$viewPath'. Error was: $_"
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to create directory '$viewPath'. Error was: $_"
     }
-    Set-Content -Path $logFullPath -Value "Successfully created directory '$viewPath'."
 
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Successfully created directory '$viewPath'."
+}
+else {
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - '$viewPath' already existed"
 }
 
-Set-Content -Path $logFullPath -Value "Script starting" 
+Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Script starting" 
 
 try 
 {
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to export Table objects to '$tablePath' for instance '$server' from database '$database'" 
 
-    Set-Content -Path $logFullPath -Value "Attempting to export Table objects to '$tablePath' for instance '$server' from database '$database'" 
+    $options = New-DbaScriptingOption
+    $options.ScriptSchema = $true
+    $options.IncludeDatabaseContext  = $true
+    $options.IncludeHeaders = $false
+    $Options.NoCommandTerminator = $false
+    $Options.ScriptBatchTerminator = $true
+    $options.DriAllConstraints = $true
+    $Options.AnsiFile = $true
+
 
     Get-DbaDbTable -SqlInstance $server -Database $database | ForEach-Object { Export-DbaScript -InputObject $_ -FilePath (Join-Path $tablePath -ChildPath "$($_.Name).sql") -ScriptingOptionsObject $options }
 }
 catch {
     Write-Error -Message "Unable to create directory '$viewPath'. Error was: $_" -ErrorAction Stop
-    Set-Content -Path $logFullPath -Value "Unable to create directory '$viewPath'. Error was: $_" 
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to create directory '$viewPath'. Error was: $_" 
 }
 
 try 
 {
-    Set-Content -Path $logFullPath -Value  "Attempting to export Stored Procedures to '$tablePath' for instance '$server' from database '$database'" 
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to export Stored Procedures to '$tablePath' for instance '$server' from database '$database'" 
 
     Get-DbaDbStoredProcedure -SqlInstance $server -Database $database -ExcludeSystemSp | ForEach-Object { Export-DbaScript -InputObject $_ -FilePath (Join-Path $storedProcedurePath -ChildPath "$($_.Name).sql") -ScriptingOptionsObject $options }
 }
 catch {
     Write-Error -Message "Unable to create directory '$viewPath'. Error was: $_" -ErrorAction Stop
-    Set-Content -Path $logFullPath -Value "Unable to create directory '$viewPath'. Error was: $_" 
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to create directory '$viewPath'. Error was: $_" 
 }
 
 try 
 {
-    Set-Content -Path $logFullPath -Value "Attempting to export Views to '$viewPath' for instance '$server' from database '$database'" 
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to export Views to '$viewPath' for instance '$server' from database '$database'" 
 
     Get-DbaDbView -SqlInstance $server -Database $database -ExcludeSystemView | ForEach-Object { Export-DbaScript -InputObject $_ -FilePath (Join-Path $viewPath -ChildPath "$($_.Name).sql") -ScriptingOptionsObject $options }
 }
 catch {
     Write-Error -Message "Unable to create directory '$viewPath'. Error was: $_" -ErrorAction Stop
-    Set-Content -Path $logFullPath -Value "Unable to create directory '$viewPath'. Error was: $_" 
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to create directory '$viewPath'. Error was: $_" 
 }
