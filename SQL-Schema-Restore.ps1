06023801acede5438a25babad9fe79239b528b14
Rich In SQL <github@codenameowl.com>
2022-10-01 13:59:21 +0100

    Update SQL-Schema-Restore.ps1
    Additional logging and checking if folder & files exists added with check to ensure files created are not empty.

--- SQL-Schema-Restore.ps1
+++ SQL-Schema-Restore.ps1
@@ -14,14 +14,6 @@ Param(
     $logDirectory
 )
 
-Function Test-FileEmpty {
-
-    Param ([Parameter(Mandatory = $true)][string]$file)
-  
-    if ((Test-Path -LiteralPath $file) -and !((Get-Content -LiteralPath $file -Raw) -match '\S')) {return $true} else {return $false}
-  
-  }
-
 $logName = 'SQL-Schema-Restore-'
 $logFileName = $logName + (Get-Date -f yyyy-MM-dd-HH-mm) + ".log"
 $logFullPath =  Join-Path $logDirectory $logFileName
@@ -39,9 +31,12 @@ if(Get-Module -ListAvailable -name dbatools)
         Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - DbaTools has now been installed."
     }
     catch {
-        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to install dbatools. The Error was: $_"
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to install dbatools. The Error was: $error"
     }
 }
+
+Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Restore was run by $env:username."
+Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Restoring schema for $database to $server from $SourceControlDirectory."
 
 Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to connect to $server."
 $svr = Connect-dbaInstance -SqlInstance $server
@@ -55,7 +50,7 @@ if(!$databaseCheck)
     New-DbaDatabase -SqlInstance $svr -Database $database
 } else {
 
-    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Dropping $database."
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $database already exists, Attempting to drop $database."
     Remove-DbaDatabase -SqlInstance $svr -Database $database -Confirm:$false
 
     Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Creating a new instance of $database."
@@ -69,7 +64,7 @@ try {
 
 }
 catch {
-    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to delete old log files from '$logFullPath'. The Error was: $_"
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to delete old log files from '$logFullPath'. The Error was: $error"
 }
 
 Write-Host -Message "$(Get-Date -f yyyy-MM-dd-HH-mm) - Script starting" -ForegroundColor Gray
@@ -84,7 +79,7 @@ if(-Not(Test-Path -Path $logFullPath -PathType Leaf))
     }
     catch 
     {
-        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to log file in '$logFullPath'. The Error was: $_"
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to log file in '$logFullPath'. The Error was: $error"
     }
 }
 
@@ -103,140 +98,186 @@ Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Set Con
 $constraintPath = $SourceControlDirectory + 'Constraints'
 Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Set Constraint Path $constraintPath"
 
-if(-Not(Test-Path -Path $tablePath))
-{
-    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $tablePath does not exist ending"
-    break
-}
-if(-Not(Test-Path -Path $viewPath))
-{
-    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $viewPath does not exist ending"
-    break
-}
-if(-Not(Test-Path -Path $storedProcedurePath))
-{
-    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $storedProcedurePath does not exist ending"
-    break
-}
-if(-Not(Test-Path -Path $constraintPath))
-{
-    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $constraintPath does not exist ending"
-    break
-}
-
 Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to get table files"
 
-$total = get-childitem $tablePath –Filter *.sql | Measure-Object | ForEach-Object{$_.Count}  
-
-if($total -ne 0)
+if(Test-Path -LiteralPath $tablePath -PathType Container)
 {
-    $tables = get-childitem $tablePath –Filter *.sql | sort-object Name
+    $tableTotal = get-childitem $tablePath –Filter *.sql | Measure-Object | ForEach-Object{$_.Count}  
 
-    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to get table files. The Error was: $_"
-
-    foreach ($table in $tables) 
+    if($tableTotal -ne 0)
     {
-        if((([IO.File]::ReadAllText($table)) -match '\S') -eq $True)
-        {
-            try {
-                Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to restore $table"        
-                Invoke-DbaQuery –SqlInstance $svr –File $table.FullName –Database $database
-            } catch 
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $tableTotal files found in $tablePath"
+
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Getting a count of files from $tablePath"
+
+        $tables = get-childitem $tablePath –Filter *.sql | sort-object Name
+
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Getting a list of files from $tablePath"
+
+        foreach ($table in $tables) 
+        {            
+            $tableFullName = $table.FullName
+
+            Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Checking to make sure $tableFullName is not empty"
+
+            if((([IO.File]::ReadAllText($table)) -match '\S') -eq $True)
             {
-                Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Restoring $table failed. The Error was: $_"
+                try {
+                    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to restore $table"        
+                    Invoke-DbaQuery –SqlInstance $svr –File $table.FullName –Database $database
+                    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Restored $table"  
+
+                } catch 
+                {
+                    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Restoring $table failed. The Error was: $error_"
+                }
+            } else {
+                Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $table is empty, no need to restore."
             }
-        } else {
-            Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $table is empty, no need to restore."
         }
+    } else {
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $tablePath exists, but no files were found skipping"
     }
+} else {
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $tablePath doesn't exist, skipping"
 }
 
 Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to get View Files"
 
-$total = get-childitem $viewPath –Filter *.sql | Measure-Object | ForEach-Object{$_.Count} 
-
-if($total -ne 0)
+if(Test-Path -LiteralPath $viewPath -PathType Container)
 {
-    $views = get-childitem $viewPath –Filter *.sql | sort-object Name
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Getting a count of files from $viewPath"
 
-    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to get view files. The Error was: $_"
+    $viewTotal = get-childitem $viewPath –Filter *.sql | Measure-Object | ForEach-Object{$_.Count} 
 
-    foreach ($view in $views) 
+    if($viewTotal -ne 0)
     {
-        if((([IO.File]::ReadAllText($view)) -match '\S') -eq $True)
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $viewTotal files found in $viewPath"
+
+        $views = get-childitem $viewPath –Filter *.sql | sort-object Name
+
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) -  Attempting to get view files from $viewPath"
+
+        foreach ($view in $views) 
         {
-            try {
+            $viewFullName = $view.FullName
+            
+            Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Checking to make sure $viewFullName is not empty"
 
-                Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to restore $view"
-                Invoke-DbaQuery –SqlInstance $svr –File $view.FullName –Database $database
-
-            } catch
+            if((([IO.File]::ReadAllText($view)) -match '\S') -eq $True)
             {
-                Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to restore $view. The Error was: $_"
+                try {
+
+                    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to restore $view"
+                    Invoke-DbaQuery –SqlInstance $svr –File $view.FullName –Database $database
+                    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Restored $view"
+
+                } catch
+                {
+                    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to restore $view. The Error was: $error"
+                }
+            }
+            else {
+                Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $view is empty, no need to restore."
             }
         }
-        else {
-            Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $view is empty, no need to restore."
-        }
+    } else {
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $viewPath exists, but no files were found skipping"
     }
+} else {
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $viewPath doesn't exist, skipping"
 }
 
 Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to get Stored Procedure Files"
 
-$total = get-childitem $storedProcedurePath –Filter *.sql | Measure-Object | ForEach-Object{$_.Count} 
-
-if($total -ne 0)
+if(Test-Path -LiteralPath $storedProcedurePath -PathType Container)
 {
-    $storedProcedures = get-childitem $storedProcedurePath –Filter *.sql | sort-object Name
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Getting a count of files from $storedProcedurePath"
 
-    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to get Stored Procedure Files. The Error was: $_"
+    $procTotal = get-childitem $storedProcedurePath –Filter *.sql | Measure-Object | ForEach-Object{$_.Count} 
 
-    foreach ($storedProcedure in $storedProcedures) 
+    if($procTotal -ne 0)
     {
-        if((([IO.File]::ReadAllText($storedProcedure)) -match '\S') -eq $True)
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $procTotal files found in $storedProcedurePath"
+
+        $storedProcedures = get-childitem $storedProcedurePath –Filter *.sql | sort-object Name
+
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to get Stored Procedures from $storedProcedures"
+
+        foreach ($storedProcedure in $storedProcedures) 
         {
-            try {         
+            $storedProcedureFullname = $storedProcedure.FullName
+            
+            Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Checking to make sure $storedProcedureFullname is not empty"
 
-                Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to restore Stored Procedure $storedProcedure"
-                Invoke-DbaQuery –SqlInstance $svr –File $storedProcedure.FullName –Database $database
-
-            } catch
+            if((([IO.File]::ReadAllText($storedProcedure)) -match '\S') -eq $True)
             {
-                Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to restore Stored Procedure $storedProcedure. The Error was: $_"
+                try {         
+
+                    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to restore Stored Procedure $storedProcedure"
+                    Invoke-DbaQuery –SqlInstance $svr –File $storedProcedure.FullName –Database $database
+                    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Restored Stored Procedure $storedProcedure"
+
+                } catch
+                {
+                    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to restore Stored Procedure $storedProcedure. The Error was: $error"
+                }
+            }
+            else {
+                Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $storedProcedure is empty, no need to restore."
             }
         }
-        else {
-            Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $storedProcedure is empty, no need to restore."
-        }
+    } else {
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $storedProcedurePath exists, but no files were found skipping"
     }
+} else {
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $storedProcedurePath doesn't exist, skipping"
 }
 
 Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to get constraint files"
 
-$total = get-childitem $constraintPath –Filter *.sql | Measure-Object | ForEach-Object{$_.Count} 
-
-if($total -ne 0)
+if(Test-Path -LiteralPath $constraintPath -PathType Container)
 {
-    $constraints = get-childitem $constraintPath –Filter *.sql | sort-object Name
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Getting a count of files from $constraintPath"
 
-    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to get constraint files. The Error was: $_"
+    $constraintTotal = get-childitem $constraintPath –Filter *.sql | Measure-Object | ForEach-Object{$_.Count} 
 
-    foreach ($constraint in $constraints) 
+    if($constraintTotal -ne 0)
     {
-        if((([IO.File]::ReadAllText($constraint)) -match '\S') -eq $True)
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $constraintTotal files found in $constraintPath"
+
+        $constraints = get-childitem $constraintPath –Filter *.sql | sort-object Name
+
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to get constraints from $constraintPath"
+
+        foreach ($constraint in $constraints) 
         {
-            try {         
+            $constraintFullname = $constraint.FullName
+            
+            Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Checking to make sure $constraintFullname is not empty"
 
-                Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to restore Constraints from $constraint"
-                Invoke-DbaQuery –SqlInstance $svr –File $constraint.FullName –Database $database
-
-            } catch
+            if((([IO.File]::ReadAllText($constraint)) -match '\S') -eq $True)
             {
-                Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to restore constraint $constraint. The Error was: $_"
+                try {         
+
+                    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Attempting to restore Constraints from $constraint"
+                    Invoke-DbaQuery –SqlInstance $svr –File $constraint.FullName –Database $database
+                    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Restored Constraints from $constraint"
+
+                } catch
+                {
+                    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - Unable to restore constraint $constraint. The Error was: $error"
+                }
+            }
+            else {
+                Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $constraint is empty, no need to restore."
             }
         }
-        else {
-            Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $constraint is empty, no need to restore."
-        }
+    } else {
+        Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $constraintPath exists, but no files were found skipping"
     }
+} else {
+    Add-Content -Path $logFullPath -Value "$(Get-Date -f yyyy-MM-dd-HH-mm) - $constraintPath doesn't exist, skipping"
 }
+
+Write-Host -Message "$(Get-Date -f yyyy-MM-dd-HH-mm) - Script Finished" -ForegroundColor Green
